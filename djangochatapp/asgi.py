42525303a6464f615c4fd0a87d936c38046ce6c8
“talhaumer” <talhumer7@gmail.com>
2022-04-16 01:07:28 +0500

    CORS Issue

--- djangochatapp/asgi.py
+++ djangochatapp/asgi.py
@@ -15,6 +15,8 @@ from django.core.asgi import get_asgi_application
 import room.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochatapp.settings')
+import django
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
