9e4b214c1f9f0e57681e5f6ba1170f665cb3db01
AzkaMinhas <azkaminhas12@gmail.com>
2022-04-15 22:40:58 +0500

    end block issue

--- djangochatapp/settings.py
+++ djangochatapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nuwqpoxa53)02++igk@il4ux%)08d+hadb$hkb5&9=z7*g83=6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['52.23.105.157']
 
