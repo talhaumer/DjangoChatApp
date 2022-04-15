feed31ecbde3bba05ec15249e4ed5f0887a2c489
“talhaumer” <talhumer7@gmail.com>
2022-04-16 01:42:55 +0500

    CORS Issue

--- djangochatapp/asgi.py
+++ djangochatapp/asgi.py
@@ -6,18 +6,28 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
+"""
+ASGI config for djangochatapp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
 import os
+from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochatapp.settings')
+django_asgi = get_asgi_application()
+
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 
 import room.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochatapp.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             room.routing.websocket_urlpatterns
